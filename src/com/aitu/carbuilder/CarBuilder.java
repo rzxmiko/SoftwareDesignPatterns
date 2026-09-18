c7db6703d0ba06beb74abaa5cdd2b9bbd6129e41
rzxmiko <250231@astanait.edu.kz>
2026-09-18 17:30:28 +0500

    перезалив в другой папке

--- src/com/aitu/carbuilder/CarBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.aitu.carbuilder;
-
-public interface CarBuilder {
-    CarBuilder setModel(String model);
-    CarBuilder setColor(String color);
-    CarBuilder setMaxSpeed(int maxSpeed);
-    CarBuilder setElectric(boolean isElectric);
-    Car build();
-}
