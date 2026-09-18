3da767b5359241715eb93b5b2540efc446e8e50f
rzxmiko <250231@astanait.edu.kz>
2026-09-19 00:59:03 +0500

    main

--- Assignment2_VehicleFactory/src/com/aitu/vehicle/Main.java
+++ Assignment2_VehicleFactory/src/com/aitu/vehicle/Main.java
@@ -1,18 +1,16 @@
 package com.aitu.vehicle;
-import com.aitu.vehicle.abstract_factory.Engine;
-import com.aitu.vehicle.abstract_factory.SafetyFeature;
+import com.aitu.vehicle.factory_method.*;
+import com.aitu.vehicle.abstract_factory.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Part A: Factory Method");
-
         runFactoryMethod(new com.aitu.vehicle.factory_method.TwoWheelerFactory());
         runFactoryMethod(new com.aitu.vehicle.factory_method.FourWheelerFactory());
 
         System.out.println("Part B: Abstract Factory");
-
-        runAbstractFactory("Two-Wheeler Family", new com.aitu.vehicle.abstract_factory.TwoWheelerFactory());
-        runAbstractFactory("Four-Wheeler Family", new com.aitu.vehicle.abstract_factory.FourWheelerFactory());
+        runAbstractFactory("2-Wheeler Family", new com.aitu.vehicle.abstract_factory.TwoWheelerFactory());
+        runAbstractFactory("4-Wheeler Family", new com.aitu.vehicle.abstract_factory.FourWheelerFactory());
     }
 
     private static void runFactoryMethod(com.aitu.vehicle.factory_method.VehicleFactory factory) {
@@ -21,11 +19,8 @@ public class Main {
 
     private static void runAbstractFactory(String title, com.aitu.vehicle.abstract_factory.VehicleFactory factory) {
         System.out.println(title);
-        Engine engine = factory.createEngine();
-        SafetyFeature safety = factory.createSafetyFeature();
-
-        engine.getSpecs();
-        safety.deploy();
+        factory.createEngine().getSpecs();
+        factory.createSafetyFeature().deploy();
         System.out.println();
     }
 }
